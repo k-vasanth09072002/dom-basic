baed58c91b5a83bf1a796ba124365f46cbc2b3de
k-vasanth09072002 <vasanthvasath74@gmail.com>
2022-12-29 10:13:25 +0530

    dom-basic-id-class-tag

---
+++ dom.js
@@ -0,0 +1,27 @@
+
+
+function  myFunction(){
+
+let heading=document.getElementById("head1");
+
+heading.innerHTML="welcome";
+
+}
+
+
+
+function myCreat(){
+    const headss= document.querySelector("p");
+    headss.innerHTML="vasanth";
+}
+
+
+
+function myBut(){
+    const sta=document.querySelector(".tell");
+    sta.innerHTML="classnamess";
+}
+
+
+
+//myCreat();
